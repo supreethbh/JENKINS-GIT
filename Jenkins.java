0c86ed12d1037de51329d21aaed4d5da108b5b9f
- <supreethbh@dxc.com>
2020-09-22 22:15:39 +0530

    Jenkins class file added

--- Jenkins.java
+++ Jenkins.java
@@ -2,7 +2,12 @@
 class Jenkins {
 
 	public static void main(String... args) {
-		System.out.println("Hello Jenkins-Java....");
+		
+		System.out.println("Hello Jenkins-Java-1....");
+
+		System.out.println("Hello Jenkins-Java-2....");
+
+		System.out.println("Hello Jenkins-Java-3....");
 	}
 
 }
